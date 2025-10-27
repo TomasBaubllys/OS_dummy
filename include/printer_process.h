5a89d8e49a35a1e92ec825a507d509bce1410a1e
godagu <goda.gutparakyte@mif.stud.vu.lt>
2025-10-27 13:08:38 +0200

    added printer process

---
+++ include/printer_process.h
@@ -0,0 +1,14 @@
+#ifndef PRINTER_PROCESS_H_INCLUDED
+#define PRINTER_PROCESS_H_INCLUDED
+
+#include "process.h"
+
+typedef struct Printer_Process{
+    Process process;
+    
+
+} Printer_Process;
+
+
+
+#endif // PROCESS_PRINTER_H_INCLUDED
