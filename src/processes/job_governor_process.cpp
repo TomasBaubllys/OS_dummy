fa4da31aed362634fe0a9e166d5ea9147b87dc7e
Goda Gutparakyte <goda@kumba.goda>
2025-12-03 01:24:23 +0200

    baby steps

--- src/processes/job_governor_process.cpp
+++ src/processes/job_governor_process.cpp
@@ -1,6 +1,7 @@
 #include "../../include/processes/job_governor_process.h"
 #include "../../include/virtual_machine.h"
 #include "../../include/kernel.h"
+#include "../../include/processes/virtual_machine_process.h"
 #include <sstream>
 
 /**
@@ -38,13 +39,7 @@ Process_State Job_Governor_Process::execute(){
 
             this -> kernel -> request_resource();
             ;*/
-        case Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_CREATE_VIRTUAL_MACHINE: 
-            // create the virtual machine
-            static Virtual_Machine* vm = (Virtual_Machine*)malloc(sizeof(Virtual_Machine));
-            init_virtual_machine(vm, this -> kernel -> get_cpu(), this -> kernel -> get_memory());
-            this -> saved_registers.ptr = this -> kernel -> get_cpu() -> ptr;
 
-            return Process_State::READY;    
         case Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_FREE_LOADER_PACKAGE_RESOURCE: {
             std::stringstream ss;
             // program length is always the length of the supervisor memory size
@@ -53,6 +48,7 @@ Process_State Job_Governor_Process::execute(){
             this -> kernel -> release_resource(Resource_Type::LOADER_PACKAGE, ss.str());
             return Process_State::READY;
         }
+
         case Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_BLOCKED_WAITING_FROM_LOADER_RESOURCE:
             if(this -> owns_resource(Resource_Type::FROM_LOADER)) {
                 this -> step = Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_CREATE_PROCESS_VIRTUAL_MACHINE;
@@ -61,7 +57,21 @@ Process_State Job_Governor_Process::execute(){
 
             this -> kernel -> request_resource(this, Resource_Type::FROM_LOADER);
             return Process_State::BLOCKED;
+        case Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_CREATE_VIRTUAL_MACHINE: 
+            // create the virtual machine
+            static Virtual_Machine* vm = (Virtual_Machine*)malloc(sizeof(Virtual_Machine));
+            init_virtual_machine(vm, this -> kernel -> get_cpu(), this -> kernel -> get_memory());
+            this -> saved_registers.ptr = this -> kernel -> get_cpu() -> ptr;
+
+            return Process_State::READY;    
+
         case Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_CREATE_PROCESS_VIRTUAL_MACHINE:
+            this -> kernel -> create_process<Virtual_Machine_Process>(this, {},  SYSTEM_USERNAME);
+            // add vm to it
+
+
+            this -> step = Job_Governor_Process_Steps::JOB_GOVERNOR_PROCESS_BLOCKED_WAITING_FOR_FROM_INTERRUPT_RESOURCE;
+            return Process_State::READY;
             /*
                 CREATE THE ACTUAL VM PROCESS AND ADD THE VM TO IT
             */   
