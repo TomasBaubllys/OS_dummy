3b39d882728eef9bc8cbec64583b9f7bf7fd3008
Tomas <tomas.baublys@mif.stud.vu.lt>
2025-12-07 16:25:16 +0200

    bruh

--- src/processes/job_governor_process.cpp
+++ src/processes/job_governor_process.cpp
@@ -3,6 +3,7 @@
 #include "../../include/kernel.h"
 #include "../../include/processes/virtual_machine_process.h"
 #include <sstream>
+#include <iostream>
 
 /**
  * 
@@ -16,6 +17,7 @@
 Job_Governor_Process::Job_Governor_Process(Kernel* kernel, Process* parent_process, std::vector<Process*> friend_processes, std::string username): 
     Process(kernel, parent_process, friend_processes, username, Process_Priorities::JOB_GOVERNOR_PRIORITY){
     this -> name = JG_NAME;
+    std::cout << "Dobze dobze..." << std::endl;
 }
 
 Job_Governor_Process::~Job_Governor_Process(){
